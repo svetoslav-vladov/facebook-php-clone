d87236a188a62780e8a0ca5860cc7b092b748cd7
Svetoslav Vladov <svetoslavladov@gmail.com>
2019-02-16 20:44:45 +0200

    Base html structure

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Facebook - PHP - Clone</title>
+</head>
+<body>
+    <diV class="wrapper wrapper-center">
+        <div class="header header_small header-blue">
+            <div class="logo">
+                <img src="#" class="logo_img" alt="facebook clone logo">
+            </div>
+            <div class="login login_top">
+                <form class="form form-inline" action="register.php" method="post">
+                    <div class="row row_form">
+                        <label for="login_redentials">Email or Phone</label>
+                        <input type="text" name="login_credentials" id="login_credentials">
+                    </div>
+                    <div class="row row_form">
+                        <label for="login_password">Email or Phone</label>
+                        <input type="password" name="login_password" id="login_password">
+                        <a href="#">Forgot account?</a>
+                    </div>
+                    <div class="row row_form">
+                        <input type="submit" class="btn btn_login btn-blue" name="btn_login" id="btn_login">
+                        <a href="#" class="link link_txt link-gray">Forgot account?</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="content">
+            <div class="info_box"></div>
+            <div class="form form_register"></div>
+        </div>
+        <div class="footer">
+            
+        </div>
+    </diV>
+</body>
+</html>
